637aafd7ecd0e158fe3ea135ed2b8dc5521d385e
David Caldwell <david@porkrind.org>
2013-05-29 19:18:11 -0700

    [XS.pm] Document everything.

--- lib/Text/Sass/XS.pm
+++ lib/Text/Sass/XS.pm
@@ -58,45 +58,170 @@ sub compile {
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Text::Sass::XS - Perl extension for blah blah blah
+Text::Sass::XS - Compile .scss files using libsass
 
 =head1 SYNOPSIS
 
+  # Object Oriented API
   use Text::Sass::XS;
-  blah blah blah
+
+  my $sass = Text::Sass::XS->new;
+  my $css = $sass->compile(".something { color: red; }");
+
+
+  # Object Oriented API w/ options
+  my $sass = Text::Sass::XS->new(include_paths   => ['some/include/path'],
+                                 image_path      => 'base_url',
+                                 output_style    => SASS_STYLE_COMPRESSED,
+                                 source_comments => 1,
+                                 dont_die        => 1);
+  my $css = $sass->compile(".something { color: red; }");
+  if (!defined $css) { # $css can be undef because 'dont_die' was set
+    warn $sass->last_error;
+  }
+
+
+
+  # Functional API
+  use Text::Sass::XS qw(:Default sass_compile);
+
+  my ($css, $err) = sass_compile(".something { color: red; }");
+  die $err if defined $err;
+
+
+  # Functional API, simple, with no error messages
+  my $css = sass_compile(".something { color: red; }");
+  die unless defined $css;
+
+
+  # Functional API w/ options
+  my ($css, $err) = sass_compile(".something { color: red; }",
+                                 include_paths => ['some/include/path'],
+                                 image_path    => 'base_url',
+                                 output_style  => SASS_STYLE_NESTED,
+                                 source_comments => 1);
+
 
 =head1 DESCRIPTION
 
-Stub documentation for Text::Sass::XS, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Text::Sass::XS provides a perl interface to libsass, a fairly complete Sass
+compiler written in C. Despite its name, Text::Sass::XS can only compile the
+newer ".scss" files.
 
-Blah blah blah.
+=head1 OBJECT ORIENTED INTERFACE
 
-=head2 EXPORT
+=over 4
 
-None by default.
+=item C<new>
 
+  $sass = Text::Sass::XS->new(options)
 
+Creates a Sass object with the specified options. Example:
+
+  $sass = Text::Sass::XS->new; # no options
+  $sass = Text::Sass::XS->new(output_style => SASS_STYLE_NESTED);
+
+=item C<compile(source_code)>
+
+  $css = $sass->compile("source code");
+
+This compiles the Sass string that is passed in the first parameter. If
+there is an error it will C<croak()>, unless the C<dont_die> option has been
+set. In that case, it will return C<undef>.
+
+=item C<last_error>
+
+  $sass->last_error
+
+Returns the error encountered by the most recent invocation of
+C<compile>. This is really only useful if the C<dont_die> option is set.
+
+C<libsass> error messages are in the form ":$line:$column $error_message" so
+you can append them to the filename for a standard looking error message.
+
+=item C<options>
+
+  $sass->options->{dont_die} = 1;
+
+Allows you to inspect or change the options after a call to C<new>.
+
+=back
+
+=head1 FUNCTIONAL INTERFACE
+
+=over 4
+
+=item C<($css, $err) = sass_compile(source_code, options)>
+
+=item C<$css = sass_compile(source_code, options)>
+
+This compiles the Sass string that is passed in the first parameter. It
+returns both the CSS and the error in list context and just the CSS in
+scalar context. One of the returned values will always be C<undef>, but
+never both.
+
+=back
+
+=head1 OPTIONS
+
+=over 4
+
+=item C<output_style>
+
+=over 4
+
+=item C<SASS_STYLE_NESTED>
+
+=item C<SASS_STYLE_COMPRESSED>
+
+=back
+
+The default is C<SASS_STYLE_NESTED>. Set to C<SASS_STYLE_COMPRESSED> to
+eliminate all whitespace (for your production CSS).
+
+=item C<source_comments>
+
+Set to C<0> (the default) and no extra comments are output. Set to C<1> and
+comments are output indicating what input line the code corresponds to.
+
+=item C<include_paths>
+
+This is an arrayref that holds the list a of paths to search (in addition to
+the current directory) when following Sass C<@import> directives.
+
+=item C<image_path>
+
+This is a string that holds the base URL. This is only used in the
+(non-standard) C<image-url()> Sass function. For example, if C<image_path>
+is set to C<'file:///tmp/a/b/c'>, then the follwoing Sass code:
+
+  .something { background-image: image-url("my/path"); }
+
+...will compile to this:
+
+  .something { background-image: url("file:///tmp/a/b/c/my/path"); }
+
+=item C<dont_die>
+
+This is only valid when used with the L<Object Oriented Interface|/"OBJECT ORIENTED INTERFACE">. It is
+described in detail there.
+
+=back
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+L<The Sass Home Page|http://sass-lang.com/>
 
-If you have a mailing list set up for your module, mention it here.
+L<The libsass Home Page|https://github.com/hcatlin/libsass>
 
-If you have a web site set up for your module, mention it here.
+L<The Text::Sass::XS Home Page|https://github.com/caldwell/Text-Sass-XS>
 
 =head1 AUTHOR
 
-David Caldwell, E<lt>david@apple.comE<gt>
+David Caldwell E<lt>david@porkrind.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -105,6 +230,5 @@ Copyright (C) 2013 by David Caldwell
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.12.4 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
