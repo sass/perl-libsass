2200d1c551c5bd2c8b7867726d78f83810880531
David Caldwell <david@porkrind.org>
2013-06-17 11:03:29 -0700

    [CSS::Sass,Changes] Version 0.4.0

--- lib/CSS/Sass.pm
+++ lib/CSS/Sass.pm
@@ -19,7 +19,7 @@ our @EXPORT = qw(
 	SASS_STYLE_COMPRESSED
 );
 
-our $VERSION = '0.3.0'; # Always keep the rightmost digit, even if it's zero (stupid perl).
+our $VERSION = '0.4.0'; # Always keep the rightmost digit, even if it's zero (stupid perl).
 
 require XSLoader;
 XSLoader::load('CSS::Sass', $VERSION);
