8fa1bb0b765bd6b0f1e8391171b09f31eed0c573
Marcel Greter <marcel.greter@ocbnet.ch>
2014-12-20 04:34:35 +0100

    Add memory leak tests for custom importer

--- t/smoke/mem-leaks.pl
+++ t/smoke/mem-leaks.pl
@@ -47,116 +47,182 @@ my $mem_usage = -e "/proc/$$/status" ? 0 : -1;
 
 warn "You need to monitor mem usage yourself!" if $mem_usage eq -1;
 
+sub run_importer_test
+{
+
+	my ($r, $err, $stat);
+
+	($r, $err, $stat) = CSS::Sass::sass_compile('@import "http://www.host.dom/red";',
+	    source_map_file => "test.css.map",
+	    importer => sub {
+	      if ($_[0] eq "http://www.host.dom/red") {
+	        return [["red.css", '@import "green";']];
+	      }
+	      if ($_[0] eq "green") {
+	        return [['http://www.host.dom/green', '@import "yellow";']];
+	      }
+	      return [['http://www.host.dom/final', 'A { color: ' . $_[0] . '; }']]; # yellow
+	    }
+	);
+
+
+	my %files = (
+
+	  "index.scss" => "
+	    \@import 'foo.scss';
+	    \@import 'bar.scss';
+	  ",
+	  "foo.scss" => "
+	    \@import 'bar2.scss';
+	    body { color: red; }
+	  ",
+	  "bar.scss" => "
+	    p { color: grey; }
+	  ",
+	  "bar2.scss" => "
+	    span { z-index: 4; }
+	  "
+	);
+
+	my $expected = "span {
+	  z-index: 4; }
+
+	body {
+	  color: red; }
+
+	p {
+	  color: grey; }
+
+	/*# sourceMappingURL=index.css.map */";
+
+	($r, $err, $stat) = CSS::Sass::sass_compile(
+	    $files{'index.scss'},
+	    input_file => "index.scss",
+	    output_file => "index.css",
+	    source_map_file => "index.css.map",
+	    importer => sub {
+	      return $_[0] unless exists $files{$_[0]};
+	      return [ [ $_[0], $files{$_[0]} ] ];
+	    }
+	);
+
+}
+
+sub run_sass_value_test
+{
+
+	foreach my $iu (1 .. 1000)
+	{
+		my $css = sass2scss("A\n  color: red;");
+		my $quoted = quote("I am a string");
+		my $unquote = unquote("'I am a string'");
+		$quoted = quote(CSS::Sass::Type->new("I am a string"));
+		$unquote = unquote(CSS::Sass::Type->new("I am a string"));
+		$unquote = unquote(CSS::Sass::Type->new("'I am a string'"));
+		$quoted = quote(quote(CSS::Sass::Type->new("'I am a string'")));
+	}
+
+	my $foo = undef;
+
+	# force stringification
+	$foo = CSS::Sass::Type->new(undef);
+	$foo = CSS::Sass::Type->new(42.35);
+	$foo = CSS::Sass::Type->new("foobar");
+	$foo = CSS::Sass::Type::Map->new("key" => "foobar");
+	$foo = CSS::Sass::Type::List->new("foo baz", 42, "bar");
+	$foo = CSS::Sass::Type::List::Comma->new("foo", "bar");
+	$foo = CSS::Sass::Type::List::Space->new("foo", "bar");
+
+	my $null = CSS::Sass::Type::Null->new;
+	my $bool = CSS::Sass::Type::Boolean->new();
+	my $bool_null = CSS::Sass::Type::Boolean->new(undef);
+	my $bool_true = CSS::Sass::Type::Boolean->new(1);
+	my $bool_false = CSS::Sass::Type::Boolean->new(0);
+	my $string = CSS::Sass::Type::String->new();
+	my $string_null = CSS::Sass::Type::String->new(undef);
+	my $string_foobar = CSS::Sass::Type::String->new('foobar');
+	my $number = CSS::Sass::Type::Number->new();
+	my $number_null = CSS::Sass::Type::Number->new(undef);
+	my $number_42 = CSS::Sass::Type::Number->new(42);
+	my $number_px = CSS::Sass::Type::Number->new(42, 'px');
+	my $number_percent = CSS::Sass::Type::Number->new(42, '%');
+	my $color = CSS::Sass::Type::Color->new();
+	my $color_rgb = CSS::Sass::Type::Color->new(42, 43, 44);
+	my $color_rgba = CSS::Sass::Type::Color->new(1, 2, 3, 0.4);
+	my $color_trans = CSS::Sass::Type::Color->new(255, 0, 128, 0);
+	my $list = CSS::Sass::Type::List->new('foo', 'bar');
+	my $list_comma = CSS::Sass::Type::List::Comma->new('foo', 'bar', 'baz');
+	my $list_space = CSS::Sass::Type::List::Space->new('foo', 'bar', 'baz');
+	my $map = CSS::Sass::Type::Map->new('foo' => 'bar');
+	my $error = CSS::Sass::Type::Error->new();
+	my $error_msg = CSS::Sass::Type::Error->new('message');
+
+	my $css_tst = $sass->compile('A { color: red; }');
+	my $css_nil1 = $sass->compile('A { color: var-pl-nil(); }');
+	my $css_nil2 = $sass->compile('$nill: null; A { color: $nill; }');
+	my $css_nil3 = $sass->compile('A { color: var-pl-big(); }');
+	my $css_nil4 = $sass->compile('A { color: quote(foobar); }');
+	my $css_int = $sass->compile('A { color: var-pl-int(); }');
+	my $css_dbl = $sass->compile('$dbl: var-pl-dbl(); A { color: $dbl; }');
+	my $css_str = $sass->compile('$str: var-pl-str(); A { color: $str; }');
+	my $css_list_a = $sass->compile('$list: var-pl-list(); A { color: nth($list, 1); }');
+	my $css_list_b = $sass->compile('$list: var-pl-list(); A { color: nth($list, -1); }');
+	my $css_map = $sass->compile('$map: var-pipe(var-pl-map()); A { color: $map; }');
+
+	$css_nil1 = $sass->compile('A { color: var-pipe(var-pl-nil()); }');
+	$css_nil2 = $sass->compile('$nill: var-pipe(null); A { color: $nill; }');
+	$css_nil3 = $sass->compile('A { color: var-pipe(var-pl-big()); }');
+	$css_nil4 = $sass->compile('A { color: var-pipe(quote(foobar)); }');
+	$css_int = $sass->compile('A { color: var-pipe(var-pl-int()); }');
+	$css_dbl = $sass->compile('$dbl: var-pipe(var-pl-dbl()); A { color: $dbl; }');
+	$css_str = $sass->compile('$str: var-pipe(var-pl-str()); A { color: $str; }');
+	$css_list_a = $sass->compile('$list: var-pipe(var-pl-list()); A { color: nth($list, 1); }');
+	$css_list_b = $sass->compile('$list: var-pipe(var-pl-list()); A { color: nth($list, -1); }');
+	$css_map = $sass->compile('$map: var-pipe(var-pl-map()); A { color: $map; }');
+
+	$css_nil1 = $sass->compile('A { color: var-pipe2(var-pl-nil()); }');
+	$css_nil2 = $sass->compile('$nill: var-pipe2(null); A { color: $nill; }');
+	$css_nil3 = $sass->compile('A { color: var-pipe2(var-pl-big()); }');
+	$css_nil4 = $sass->compile('A { color: var-pipe2(quote(foobar)); }');
+	$css_int = $sass->compile('A { color: var-pipe2(var-pl-int()); }');
+	$css_dbl = $sass->compile('$dbl: var-pipe2(var-pl-dbl()); A { color: $dbl; }');
+	$css_str = $sass->compile('$str: var-pipe2(var-pl-str()); A { color: $str; }');
+	$css_list_a = $sass->compile('$list: var-pipe2(var-pl-list()); A { color: nth($list, 1); }');
+	$css_list_b = $sass->compile('$list: var-pipe2(var-pl-list()); A { color: nth($list, -1); }');
+	$css_map = $sass->compile('$map: var-pipe2(var-pl-map()); A { color: $map; }');
+
+	$css_nil1 = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-nil())); }');
+	$css_nil2 = $sass->compile('$nill: var-pipe(var-pipe2(null)); A { color: $nill; }');
+	$css_nil3 = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-big())); }');
+	$css_nil4 = $sass->compile('A { color: var-pipe(var-pipe2(quote(foobar))); }');
+	$css_int = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-int())); }');
+	$css_dbl = $sass->compile('$dbl: var-pipe(var-pipe2(var-pl-dbl())); A { color: $dbl; }');
+	$css_str = $sass->compile('$str: var-pipe(var-pipe2(var-pl-str())); A { color: $str; }');
+	$css_list_a = $sass->compile('$list: var-pipe(var-pipe2(var-pl-list())); A { color: nth($list, 1); }');
+	$css_list_b = $sass->compile('$list: var-pipe(var-pipe2(var-pl-list())); A { color: nth($list, -1); }');
+	$css_map = $sass->compile('$map: var-pipe(var-pipe2(var-pl-map())); A { color: $map; }');
+
+	$css_nil1 = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-nil())); }');
+	$css_nil2 = $sass->compile('$nill: var-pipe2(var-pipe(null)); A { color: $nill; }');
+	$css_nil3 = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-big())); }');
+	$css_nil4 = $sass->compile('A { color: var-pipe2(var-pipe(quote(foobar))); }');
+	$css_int = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-int())); }');
+	$css_dbl = $sass->compile('$dbl: var-pipe2(var-pipe(var-pl-dbl())); A { color: $dbl; }');
+	$css_str = $sass->compile('$str: var-pipe2(var-pipe(var-pl-str())); A { color: $str; }');
+	$css_list_a = $sass->compile('$list: var-pipe2(var-pipe(var-pl-list())); A { color: nth($list, 1); }');
+	$css_list_b = $sass->compile('$list: var-pipe2(var-pipe(var-pl-list())); A { color: nth($list, -1); }');
+	$css_map = $sass->compile('$map: var-pipe2(var-pipe(var-pl-map())); A { color: $map; }');
+
+}
+
 # 200000 proved to be a very safe value
 # leaking one byte should be exposed by then
 for (my $i = 0; $i < 200000; $i ++)
 {
 
-	{
 
-		foreach my $iu (1 .. 1000)
-		{
-			my $css = sass2scss("A\n  color: red;");
-			my $quoted = quote("I am a string");
-			my $unquote = unquote("'I am a string'");
-			$quoted = quote(CSS::Sass::Type->new("I am a string"));
-			$unquote = unquote(CSS::Sass::Type->new("I am a string"));
-			$unquote = unquote(CSS::Sass::Type->new("'I am a string'"));
-			$quoted = quote(quote(CSS::Sass::Type->new("'I am a string'")));
-		}
-
-		my $foo = undef;
-
-		# force stringification
-		$foo = CSS::Sass::Type->new(undef);
-		$foo = CSS::Sass::Type->new(42.35);
-		$foo = CSS::Sass::Type->new("foobar");
-		$foo = CSS::Sass::Type::Map->new("key" => "foobar");
-		$foo = CSS::Sass::Type::List->new("foo baz", 42, "bar");
-		$foo = CSS::Sass::Type::List::Comma->new("foo", "bar");
-		$foo = CSS::Sass::Type::List::Space->new("foo", "bar");
-
-		my $null = CSS::Sass::Type::Null->new;
-		my $bool = CSS::Sass::Type::Boolean->new();
-		my $bool_null = CSS::Sass::Type::Boolean->new(undef);
-		my $bool_true = CSS::Sass::Type::Boolean->new(1);
-		my $bool_false = CSS::Sass::Type::Boolean->new(0);
-		my $string = CSS::Sass::Type::String->new();
-		my $string_null = CSS::Sass::Type::String->new(undef);
-		my $string_foobar = CSS::Sass::Type::String->new('foobar');
-		my $number = CSS::Sass::Type::Number->new();
-		my $number_null = CSS::Sass::Type::Number->new(undef);
-		my $number_42 = CSS::Sass::Type::Number->new(42);
-		my $number_px = CSS::Sass::Type::Number->new(42, 'px');
-		my $number_percent = CSS::Sass::Type::Number->new(42, '%');
-		my $color = CSS::Sass::Type::Color->new();
-		my $color_rgb = CSS::Sass::Type::Color->new(42, 43, 44);
-		my $color_rgba = CSS::Sass::Type::Color->new(1, 2, 3, 0.4);
-		my $color_trans = CSS::Sass::Type::Color->new(255, 0, 128, 0);
-		my $list = CSS::Sass::Type::List->new('foo', 'bar');
-		my $list_comma = CSS::Sass::Type::List::Comma->new('foo', 'bar', 'baz');
-		my $list_space = CSS::Sass::Type::List::Space->new('foo', 'bar', 'baz');
-		my $map = CSS::Sass::Type::Map->new('foo' => 'bar');
-		my $error = CSS::Sass::Type::Error->new();
-		my $error_msg = CSS::Sass::Type::Error->new('message');
-
-		my $css_tst = $sass->compile('A { color: red; }');
-		my $css_nil1 = $sass->compile('A { color: var-pl-nil(); }');
-		my $css_nil2 = $sass->compile('$nill: null; A { color: $nill; }');
-		my $css_nil3 = $sass->compile('A { color: var-pl-big(); }');
-		my $css_nil4 = $sass->compile('A { color: quote(foobar); }');
-		my $css_int = $sass->compile('A { color: var-pl-int(); }');
-		my $css_dbl = $sass->compile('$dbl: var-pl-dbl(); A { color: $dbl; }');
-		my $css_str = $sass->compile('$str: var-pl-str(); A { color: $str; }');
-		my $css_list_a = $sass->compile('$list: var-pl-list(); A { color: nth($list, 1); }');
-		my $css_list_b = $sass->compile('$list: var-pl-list(); A { color: nth($list, -1); }');
-		my $css_map = $sass->compile('$map: var-pipe(var-pl-map()); A { color: $map; }');
-
-		$css_nil1 = $sass->compile('A { color: var-pipe(var-pl-nil()); }');
-		$css_nil2 = $sass->compile('$nill: var-pipe(null); A { color: $nill; }');
-		$css_nil3 = $sass->compile('A { color: var-pipe(var-pl-big()); }');
-		$css_nil4 = $sass->compile('A { color: var-pipe(quote(foobar)); }');
-		$css_int = $sass->compile('A { color: var-pipe(var-pl-int()); }');
-		$css_dbl = $sass->compile('$dbl: var-pipe(var-pl-dbl()); A { color: $dbl; }');
-		$css_str = $sass->compile('$str: var-pipe(var-pl-str()); A { color: $str; }');
-		$css_list_a = $sass->compile('$list: var-pipe(var-pl-list()); A { color: nth($list, 1); }');
-		$css_list_b = $sass->compile('$list: var-pipe(var-pl-list()); A { color: nth($list, -1); }');
-		$css_map = $sass->compile('$map: var-pipe(var-pl-map()); A { color: $map; }');
-
-		$css_nil1 = $sass->compile('A { color: var-pipe2(var-pl-nil()); }');
-		$css_nil2 = $sass->compile('$nill: var-pipe2(null); A { color: $nill; }');
-		$css_nil3 = $sass->compile('A { color: var-pipe2(var-pl-big()); }');
-		$css_nil4 = $sass->compile('A { color: var-pipe2(quote(foobar)); }');
-		$css_int = $sass->compile('A { color: var-pipe2(var-pl-int()); }');
-		$css_dbl = $sass->compile('$dbl: var-pipe2(var-pl-dbl()); A { color: $dbl; }');
-		$css_str = $sass->compile('$str: var-pipe2(var-pl-str()); A { color: $str; }');
-		$css_list_a = $sass->compile('$list: var-pipe2(var-pl-list()); A { color: nth($list, 1); }');
-		$css_list_b = $sass->compile('$list: var-pipe2(var-pl-list()); A { color: nth($list, -1); }');
-		$css_map = $sass->compile('$map: var-pipe2(var-pl-map()); A { color: $map; }');
-
-		$css_nil1 = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-nil())); }');
-		$css_nil2 = $sass->compile('$nill: var-pipe(var-pipe2(null)); A { color: $nill; }');
-		$css_nil3 = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-big())); }');
-		$css_nil4 = $sass->compile('A { color: var-pipe(var-pipe2(quote(foobar))); }');
-		$css_int = $sass->compile('A { color: var-pipe(var-pipe2(var-pl-int())); }');
-		$css_dbl = $sass->compile('$dbl: var-pipe(var-pipe2(var-pl-dbl())); A { color: $dbl; }');
-		$css_str = $sass->compile('$str: var-pipe(var-pipe2(var-pl-str())); A { color: $str; }');
-		$css_list_a = $sass->compile('$list: var-pipe(var-pipe2(var-pl-list())); A { color: nth($list, 1); }');
-		$css_list_b = $sass->compile('$list: var-pipe(var-pipe2(var-pl-list())); A { color: nth($list, -1); }');
-		$css_map = $sass->compile('$map: var-pipe(var-pipe2(var-pl-map())); A { color: $map; }');
-
-		$css_nil1 = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-nil())); }');
-		$css_nil2 = $sass->compile('$nill: var-pipe2(var-pipe(null)); A { color: $nill; }');
-		$css_nil3 = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-big())); }');
-		$css_nil4 = $sass->compile('A { color: var-pipe2(var-pipe(quote(foobar))); }');
-		$css_int = $sass->compile('A { color: var-pipe2(var-pipe(var-pl-int())); }');
-		$css_dbl = $sass->compile('$dbl: var-pipe2(var-pipe(var-pl-dbl())); A { color: $dbl; }');
-		$css_str = $sass->compile('$str: var-pipe2(var-pipe(var-pl-str())); A { color: $str; }');
-		$css_list_a = $sass->compile('$list: var-pipe2(var-pipe(var-pl-list())); A { color: nth($list, 1); }');
-		$css_list_b = $sass->compile('$list: var-pipe2(var-pipe(var-pl-list())); A { color: nth($list, -1); }');
-		$css_map = $sass->compile('$map: var-pipe2(var-pipe(var-pl-map())); A { color: $map; }');
-
-	}
+	run_sass_value_test();
+	run_importer_test();
 
 	if ($mem_usage != -1 && $i % 100 == 0)
 	{
