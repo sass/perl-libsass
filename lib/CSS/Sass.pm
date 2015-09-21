33ce07c1e65d22fc31a1921c72a80129c722b7ed
Marcel Greter <marcel.greter@ocbnet.ch>
2015-09-21 17:50:41 +0200

    Update to latest libsass release (3.3.0-beta2)

--- lib/CSS/Sass.pm
+++ lib/CSS/Sass.pm
@@ -69,7 +69,7 @@ our @EXPORT = qw(
 	SASS2SCSS_CONVERT_COMMENT
 );
 
-our $VERSION = "3.3.0_01";
+our $VERSION = "3.3.0_02";
 
 require XSLoader;
 XSLoader::load('CSS::Sass', $VERSION);
--- lib/CSS/Sass/Watchdog.pm
+++ lib/CSS/Sass/Watchdog.pm
@@ -25,7 +25,7 @@ use CSS::Sass;
 
 ################################################################################
 package CSS::Sass::Watchdog;
-our $VERSION = "3.3.0_01";
+our $VERSION = "3.3.0_02";
 ################################################################################
 
 use Exporter 'import'; # gives you Exporter's import() method directly
