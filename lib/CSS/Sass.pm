1cdc83106bb7922fc1131ba31616458b41fcbc72
Marcel Greter <marcel.greter@ocbnet.ch>
2017-10-16 01:25:14 +0200

    Bump version to 3.4.7

--- lib/CSS/Sass.pm
+++ lib/CSS/Sass.pm
@@ -69,7 +69,7 @@ our @EXPORT = qw(
 	SASS2SCSS_CONVERT_COMMENT
 );
 
-our $VERSION = "3.4.6";
+our $VERSION = "3.4.7";
 
 require XSLoader;
 XSLoader::load('CSS::Sass', $VERSION);
--- lib/CSS/Sass/Watchdog.pm
+++ lib/CSS/Sass/Watchdog.pm
@@ -25,7 +25,7 @@ use CSS::Sass;
 
 ################################################################################
 package CSS::Sass::Watchdog;
-our $VERSION = "3.4.6";
+our $VERSION = "3.4.7";
 ################################################################################
 
 use Exporter 'import'; # gives you Exporter's import() method directly
